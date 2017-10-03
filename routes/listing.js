7eff5925f644f95de338083ec90786ff6c938e4d
Brandon Karl <brandon@bkarl.com>
2017-10-03 00:50:56 -0400

    refactored listings

--- routes/listing.js
+++ routes/listing.js
@@ -5,10 +5,12 @@ import AWS from 'aws-sdk'
 
 import Listing from '../models/listing'
 import User from '../models/user'
+// import each from 'promise-each'
 
 const router = express.Router()
 
-// import Promise from 'bluebird'
+
+import Promise from 'bluebird'
 
 var upload = multer()
 
@@ -16,82 +18,29 @@ const s3 = new AWS.S3()
 
 router.post('/', upload.array('photos'), async (req, res) => {
   const { name, description, brand, condition, userId } = req.body
-  User.findById(userId)
-  .then(user => {
-    if(user) {
-      const newListing = {
-        name,
-        description,
-        brand,
-        condition,
-        user // Put the user in the listing
-      }
-      Listing.create(newListing)
-      .then(async listing => {
-       const promiseMap = req.files.map((image, key) => {
-         return new Promise((resolve, reject) => {
-           sharp(image.buffer).resize(200, 200).toBuffer().then(data => {
-             const params = { Body: data, Bucket: `gearhubbucket1/${listing.id}`, Key: `photo${key}.png`}
-             s3.putObject(params, (err, data) => {
-               if(err){
-                 res.send(err)
-                 reject(err)
-               }
-               else {
-                 //listing.photos.push(`https://s3.amazonaws.com/gearhubbucket1/${listing.id}/photo${key}.png`)
-                 resolve(`https://s3.amazonaws.com/gearhubbucket1/${listing.id}/photo${key}.png`)
-               }
-             })
-           })
-         })
-       })
+  let user = await User.findById(userId)
+  if(user) {
+    const newListing = {
+      name,
+      description,
+      brand,
+      condition,
+      user // Put the user in the listing
+    }
 
-      //  var as = await Promise.resolve(promiseMap[0])
+    let listing = await Listing.create(newListing)
 
-      //  Promise.all(promiseMap)
-      //  .then(arrOfResolved => {
-      //    listing.save((err, listing) => {
-      //      user.listings.push(listing)
-      //      user.save((err, user) => {
-      //        return res.json(listing)
-      //      })
-      //    })
-      //  })
+    listing.photos = await Promise.all(req.files.map(async (image, key) => {
+       const buffer = await sharp(image.buffer).resize(200, 200).toBuffer()
+       await s3.putObject({ Body: buffer, Bucket: `gearhubbucket1/${user.id}/listings/${listing.id}`, Key: `photo${key}.png`}).promise()
+       return `https://s3.amazonaws.com/gearhubbucket1/${user.id}/listings/${listing.id}/photo${key}.png`
+    }));
 
-      // var vals = []
-      // for(var i = 0; i < promiseMap.length(); i++) {
-      //   vals.push(await promiseMap[i])
-      //   console.log(vals)
-      // }
-      //
-      // listing.save((err, listing) => {
-      //      user.listings.push(listing)
-      //      user.save((err, user) => {
-      //        return res.json(listing)
-      //      })
-      //    })
-
-      // console.log(promiseMap.length)
-      // for(var i = 0; i < promiseMap.length; i++) {
-      //   console.log(await promiseMap[i])
-      // }
-
-      for(let promise in promiseMap) {
-        listing.photos.push(await promiseMap[promise])
-      }
-
-      listing = await listing.save()
-      user.listings.push(listing)
-      await user.save()
-      return res.json(listing)
-
-
-      })
-      .catch(err => { return res.status(500).json( { errors: { form: err } } ) }) // Error in creation
-    } else
-      return res.status(403).json( { errors: 'Must be a registered user to post '}) // Only get here if front-end issue
-  })
-  .catch(err => { return res.status(500).json( { errors: { form: 'Server Error user' } } ) }) // Error in lookup
+    listing = await listing.save()
+    user.listings.push(listing)
+    await user.save()
+    return res.json(listing)
+  } else return res.status(403).json( { errors: 'Must be a registered user to post '}) // Only get here if front-end issue
 })
 
 export default router
